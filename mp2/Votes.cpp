a55108479b8c4ad740953061fcf11e8e8752fff0
JRBenson01 <mc.nyancat5400@gmail.com>
2019-02-17 19:02:35 -0500

    more work done on mp2

--- mp2/Votes.cpp
+++ mp2/Votes.cpp
@@ -1,10 +1,18 @@
 #include "Votes.h"
+#include <string>
+#include <iostream>
 
 using namespace std;
 
+//=====+ Candidate Class +=====
 void Candidate::SetVotes(int newVotes)
 {
 	votes = newVotes;
+}
+
+void Candidate::SetVoteShare(double newVoteShare)
+{
+	voteShare = newVoteShare;
 }
 
 int Candidate::GetVotes()
@@ -15,4 +23,47 @@ int Candidate::GetVotes()
 string Candidate::GetName()
 {
 	return name;
+}
+
+double Candidate::GetVoteShare()
+{
+	return voteShare;
+}
+
+bool Candidate::operator>(const Candidate& rightSideObj)
+{
+	return this->votes > rightSideObj.votes;
+}
+
+bool Candidate::operator<(const Candidate& rightSideObj)
+{
+	return this->votes < rightSideObj.votes;
+}
+
+
+//=====+ VoteList class +=====
+void VoteList::AddCandidate(Candidate candToAdd)
+{
+	candList.push_back(candToAdd);
+	totalVotes += candToAdd.GetVotes();
+}
+
+void VoteList::CalcVoteShare()
+{
+	for(int i = 0; i < candList.size; i++)
+	{
+		double tempVoteS = candList[i].GetVoteShare()/totalVotes;
+		candList[i].SetVoteShare(tempVoteS);
+	}
+}
+
+void VoteList::SortCands()
+{
+	
+}
+
+void VoteList::ProcessVotes()
+{
+	CalcVoteShare();
+	SortCands();
 }
--- mp2/Votes.h
+++ mp2/Votes.h
@@ -9,17 +9,29 @@ class Candidate
 private:
 	int votes;
 	string name;
+	double voteShare;
 public:
 	Candidate(string n = "Doe", int v = 0) : votes(v), name(n) {}
 	void SetVotes(int newVotes);
+	void SetVoteShare(double newVoteShare);
 	int GetVotes();
 	string GetName();
+	double GetVoteShare();
+
+	bool operator>(const Candidate& rightSideObj);
+	bool operator<(const Candidate& rightSideObj);
 };
 
 class VoteList
 {
+	friend istream& operator>>(istream& givenStream, VoteList& righSideObj);
+	friend ostream& operator<<(ostream& givenStream, const VoteList& rightSideObj);
 private:
 	vector<Candidate> candList;
+	int totalVotes = 0;
 public:
-	
+	void AddCandidate(Candidate candToAdd);
+	void CalcVoteShare();
+	void SortCands();
+	void ProcessVotes();
 };
